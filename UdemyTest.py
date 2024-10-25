b98bc5d99fa1b3fce04b734e7c6e9bf09a9022da
Kapil <48823280+PUJARKN@users.noreply.github.com>
2024-10-25 12:52:08 +0530

    Update UdemyTest.py

--- UdemyTest.py
+++ UdemyTest.py
@@ -1,11 +1,2 @@
-def myfunc(*args):
-    return sum(args)* 0.5
-
-print(myfunc(21,35,35,5,45,23,2,3,4))
-
-
-def myfunc(*args):
-    return [num for num in args if num % 2 == 0]
-#myfunc(23,24,3412,122,2,1,9)
-
-
+def test_dummy():
+    pass
